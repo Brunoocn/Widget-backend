cc9ff806b661d59550fc486dc0fee52f142e1b94
Bruno Caneo <brunocaneo3@gmail.com>
2022-05-14 00:12:01 -0300

    Adding tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,5 @@
-import nodemailer from "nodemailer";
 import express from "express";
-import { prisma } from "./prisma";
+
 import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
